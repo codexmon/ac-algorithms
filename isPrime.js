b294b0728750c56ea58c475ea32e193bd42d7755
rev <rev@anniecannons.com>
2018-10-10 11:04:49 -0700

    completed isPrime exercise

--- isPrime.js
+++ isPrime.js
@@ -31,6 +31,46 @@ console.log(result) // false
 
 **/
 
+//prime number is any number divisible by itself and 1. 
+
+//input: any number between 1 and 2^16
+//output: boolean
+//needs variable to store the output?
+//needs if else conditional statement
+//needs a modulus
+//needs return statement
+
+//group pseudo code
+//input: any number between 1 and 2^16
+//output: boolean
+//for loop that starts at 1 and goes up to the input number
+	// conditional statement: if statement to check if the input number is 1 or 2
+	// checks if the input number is divisible by i with 0 as its remainder, if true, we want it to return false
+	//in else statement, return true
+
+//group code
 function isPrime(num) {
-	// Your code
+	
+	if (num === 1){
+		return false
+	} 
+	if (num === 2){
+		return true;
+	}
+	for (var i = 2; i < num; i++){
+		if (num % i === 0){
+			return false;
+		}
+	}
+	return true;
+}
+
+//Alternate code from online
+function isPrime(num) {
+    for(var i = 2; i < num; i++) {
+        if(num % i === 0) {
+            return false;
+        }
+    }
+    return num > 1;
 }
