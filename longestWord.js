79ebd979daf4328007b49565e7552a0d1a7f776b
rev <rev@anniecannons.com>
2019-09-18 11:45:12 -0700

    longestWord update

--- longestWord.js
+++ longestWord.js
@@ -19,15 +19,24 @@
 // findLongestWord("What if we try a super-long word such as otorhinolaryngology") should return 19.
 
 //input: a string
-//output: a number, the length of the longest word
-//purpose: to be able to count the number of latters in a word in a multi-word string, and to find the longest word
+//output: a number, the length of the longest word.
+//purpose: to be able to count the number of letters in a word in a multi-word string, and to find the longest word
+
+//We need to look at the length of each word, we need to compare the length of each word with the others in the string. If a word is longer than the one that comes before it, we will replace the value of its length.
+//Tools: a variable to store the length of the longest string
+//.split() to change our string into an array
+// For loop: run through the words
+  //Conditional statatement: check to see if the next word is longer than the word that came before it.
+    //If true, I want to replace the value of the variable with the length of the longer word.
+
+
 
 //create a variable to store the longest string
 //variable to store our array
 //.split to turn our string into an array
 //for loop to loop through an array of words
   //conditional statement to test to see the new word we are testing is longer than the one stored in our variable. if it is, it replaces it with the new word
-//return the variable stroing the longest word
+//return the variable string the longest word
 
 
 
