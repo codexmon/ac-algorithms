6952edfaf5ad8901b9c8fadd0be47d809fe9b3d9
rev <rev@anniecannons.com>
2019-09-24 12:47:38 -0700

    dnaPairing switch toUpperCase

--- dnaPairing.js
+++ dnaPairing.js
@@ -14,6 +14,13 @@
 //output: a nested array
 //purpose: match each element with its base pair, resulting in an array of base pairs
 
+//Create a variable that stores the transformation of the input string into an array, .split("")
+//Create a variable that stores an empty array, this will eventually be our output
+//Create a for loop to run through the array of letters
+  //Create a conditional statement that checks to see if the current letter we are on in the loop is equal to one of the 4 letters.
+    //If true, push an array with the two strings into our variable storing the array
+  //Return the parent array, variable.
+
 function pairElement(str) {
   //variable to store split string into an array
   var strArray = str.split('');
@@ -37,13 +44,62 @@ function pairElement(str) {
       var basePair = ["T", "A"];
       parentArray.push(basePair);
     }
+
+    //Alternate Code
+    if (strArray[i].toUpperCase() === "G"){
+      parentArray.push(["G", "C"]);
+    } else if (strArray[i].toUpperCase() === "C"){
+      parentArray.push(["C", "G"]);
+    } else if (strArray[i].toUpperCase() === "A"){
+      parentArray.push(["A", "T"]);
+    } else if (strArray[i].toUpperCase() === "T"){
+      parentArray.push(["T", "A"]);
+    }
+
   } 
   //return the parent array outside of conditional statement or else it will only run once instead of looping through
   return parentArray;
 }
 
-var results = pairElement("GCG");
-console.log(results);
+// Alternate Function using the Switch Keyword from Free Code Camp
+//Switch is useful if there are more than 15 (lots and lots of) different options and you do not want to write the condition again and again. 
+function pairElement(str) {
+  // Return each strand as an array of two elements, the original and the pair.
+  var paired = [];
+
+  // Function to check with strand to pair.
+  var search = function(char) {
+    switch (char) {
+      case "A":
+        paired.push(["A", "T"]);
+        break;
+      case "T":
+        paired.push(["T", "A"]);
+        break;
+      case "C":
+        paired.push(["C", "G"]);
+        break;
+      case "G":
+        paired.push(["G", "C"]);
+        break;
+    }
+  };
+
+  // Loops through the input and pair.
+  for (var i = 0; i < str.length; i++) {
+    search(str[i].toUpperCase());
+  }
+
+  return paired;
+}
+
+// test here
+pairElement("GCG");
+
+
+var results1 = pairElement("GCG");
+var results2 = pairElement("GACTA");
+console.log(results1);
 
 //TEST CASES
 //pairElement("ATCGA") should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
